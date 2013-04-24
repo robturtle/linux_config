14dd0f740a64b561e3032fa334a1cb650745ad3a
robturtle <jeremyrobturtle@gmail.com>
2013-04-25 01:07:54 +0800

    update newpy.sh, new update_bin.sh

--- bin/newpy.sh
+++ bin/newpy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 [ "$1" != "" ] && file="$1".py || exit
-echo "#!/usr/bin/python" > "$file"
+echo "#!/usr/bin/env python" > "$file"
 echo "# Filename: ${1}.py"    >> "$file"
 chmod +x "$file"
---
+++ bin/update_bin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp -a ~/bin ~/git/linux_config/
+cp -a ~/git/linux_config/bin ~/
+cd ~/git/linux_config
+git add .
+git status
