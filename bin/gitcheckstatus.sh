a4be38f87772ef4ded048ced4c6489d262d96d4b
LIU Yang <JeremyRobturtle@gmail.com>
2014-04-03 21:08:49 +0800

    Make message clear
    Get rid of color settings for god know why bug
    showing the color code instead of change the text
    color

--- bin/gitcheckstatus.sh
+++ bin/gitcheckstatus.sh
@@ -9,9 +9,9 @@ cd "$gitpath"
 for d in $(ls); do
     test -d "$d" && cd "$d" || continue
     test -d .git || continue
-    echo -e "\e[0;34m*********************************************************"
-    echo -e "Repository $d:\e[0;37m"
-    git status
-    echo -e "\e[0;34m*********************************************************\e[0;37m\n"
+    echo -e "*********************************************************"
+    echo -e "Repository $d:"
+    git status -s
+    echo -e "*********************************************************\n"
     cd ..
 done
